ebe8138392a0f94b15dc6044c948dedb9ac97896
thitaya <tsuaimak@gmail.com>
2018-05-30 17:27:12 +0700

    เพิ่ม

---
+++ front-app/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import firebase from 'firebase'
+
+import Header from './components/Header'
+import Contents from './components/Content'
+import Footer from './components/Footer'
+import './App.css';
+const config = {
+  apiKey: "AIzaSyAPHQAAG6znTUbG6PL7Oiw2hwI50lrPtQY",
+  authDomain: "daily-report-14ce7.firebaseapp.com",
+  databaseURL: "https://daily-report-14ce7.firebaseio.com",
+  projectId: "daily-report-14ce7",
+  storageBucket: "daily-report-14ce7.appspot.com",
+  messagingSenderId: "545247969478"
+}
+firebase.initializeApp(config)
+
+class App extends Component {
+  render() {
+    console.log(firebase.app().name)
+    return (
+      <div >
+        <Header />
+        <Contents />
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ front-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header-top"> 
+        <h1 className="h1-font"><i className="fa fa-comments-o">Daily Report Hackathon</i></h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
